5923235a8c615a7f4d83c008a477944c83a9d109
Harsha matur <harshasmatur.austratech@gmail.com>
2026-02-19 22:18:16 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='min-h-screen flex items-center justify-center'>
+
+      <h1 className='text-4xl font-bold text-black'>Hi Here We are Building the Edu-Connect the complete Suitcase of Student </h1> 
+      
+    </div>   
+    </>
+  )
+}
+
+export default App
